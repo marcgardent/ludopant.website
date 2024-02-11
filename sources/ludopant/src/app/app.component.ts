cd8b6c9efd2185a166e90a90e022ff3963ca2f02
marcgardent <marcgardent@yahoo.fr>
2024-02-11 17:46:01 +0100

    fix progressbar

--- sources/ludopant/src/app/app.component.ts
+++ sources/ludopant/src/app/app.component.ts
@@ -3,6 +3,7 @@ import { RouterOutlet } from '@angular/router';
 import {SocialnetworkComponent} from "./sections/socialnetwork/socialnetwork.component";
 import {ProjectsComponent} from "./sections/projects/projects.component";
 import {DashboardModel} from "./sections/projects/dashboard/models";
+import {data_projects} from "../data/data_projects";
 
 @Component({
   selector: 'app-root',
@@ -13,67 +14,5 @@ import {DashboardModel} from "./sections/projects/dashboard/models";
 })
 export class AppComponent {
   title = 'ludopant';
-
-  projects : Array<DashboardModel> = [
-    {
-      title : "Les Toits de Paris : Une Rencontre En Cache Une Autre [CJS-S1E1]",
-      cover : "assets/img/items/celeste_et_jules-les_toits_de_paris-e1s1.webp",
-      status : "édition en accès anticipé disponible !",
-      details : [
-        { name : "Scénographies", progress : "100%"},
-        { name : "Storyboards", progress : "100%"},
-        { name : "Crayonnés", progress : "100%"},
-        { name : "Couleurs", progress : "100%"},
-        { name : "Finitions", progress : "100%"},
-      ]
-    },
-    {
-      title : "Jardins Suspendus : Une Porte Vers l'Espace [CJS-S1E2]",
-      cover : "assets/img/items/CJS-S1E2.webp",
-      status : "En cours de conception",
-      details : [
-        { name : "Scénographies", progress : "80%"},
-        { name : "Storyboards", progress : "20%"},
-        { name : "Crayonnés", progress : "10%"},
-        { name : "Couleurs", progress : "0%"},
-        { name : "Finitions", progress : "0%"},
-      ]
-    },
-    {
-      title : "Ville Fantôme : la clef de nos espoirs [CJS-S1E3]",
-      cover : "assets/img/items/CJS-S1E3.webp",
-      status : "À Planifier",
-      details : [
-        { name : "Scénographies", progress : "0%"},
-        { name : "Storyboards", progress : "0%"},
-        { name : "Crayonnés", progress : "0%"},
-        { name : "Couleurs", progress : "0%"},
-        { name : "Finitions", progress : "0%"},
-      ]
-    },
-    {
-      title : "Odyssey : Le Salaire de la peur [CJS-S1E4]",
-      cover : "assets/img/items/CJS-S1E4.webp",
-      status : "À Planifier",
-      details : [
-        { name : "Scénographies", progress : "0%"},
-        { name : "Storyboards", progress : "0%"},
-        { name : "Crayonnés", progress : "0%"},
-        { name : "Couleurs", progress : "0%"},
-        { name : "Finitions", progress : "0%"},
-      ]
-    },
-    {
-      title : "La Résistance [CJS-S1E5]",
-      cover : "assets/img/items/CJS-S1E5.webp",
-      status : "À Planifier",
-      details : [
-        { name : "Scénographies", progress : "0%"},
-        { name : "Storyboards", progress : "0%"},
-        { name : "Crayonnés", progress : "0%"},
-        { name : "Couleurs", progress : "0%"},
-        { name : "Finitions", progress : "0%"},
-      ]
-    },
-  ]
+  projects : Array<DashboardModel> = data_projects;
 }
---
+++ sources/ludopant/src/data/data_projects.ts
@@ -0,0 +1,64 @@
+import {DashboardModel} from "../app/sections/projects/dashboard/models";
+
+export const data_projects : Array<DashboardModel> = [
+  {
+    title : "Les Toits de Paris : Une Rencontre En Cache Une Autre [CJS-S1E1]",
+    cover : "assets/img/items/celeste_et_jules-les_toits_de_paris-e1s1.webp",
+    status : "édition en accès anticipé disponible !",
+    details : [
+      { name : "Scénographies", progress : "100%"},
+      { name : "Storyboards", progress : "100%"},
+      { name : "Crayonnés", progress : "100%"},
+      { name : "Couleurs", progress : "100%"},
+      { name : "Finitions", progress : "100%"},
+    ]
+  },
+  {
+    title : "Jardins Suspendus : Une Porte Vers l'Espace [CJS-S1E2]",
+    cover : "assets/img/items/CJS-S1E2.webp",
+    status : "En cours de conception",
+    details : [
+      { name : "Scénographies", progress : "80%"},
+      { name : "Storyboards", progress : "20%"},
+      { name : "Crayonnés", progress : "10%"},
+      { name : "Couleurs", progress : "0%"},
+      { name : "Finitions", progress : "0%"},
+    ]
+  },
+  {
+    title : "Ville Fantôme : la clef de nos espoirs [CJS-S1E3]",
+    cover : "assets/img/items/CJS-S1E3.webp",
+    status : "À Planifier",
+    details : [
+      { name : "Scénographies", progress : "0%"},
+      { name : "Storyboards", progress : "0%"},
+      { name : "Crayonnés", progress : "0%"},
+      { name : "Couleurs", progress : "0%"},
+      { name : "Finitions", progress : "0%"},
+    ]
+  },
+  {
+    title : "Odyssey : Le Salaire de la peur [CJS-S1E4]",
+    cover : "assets/img/items/CJS-S1E4.webp",
+    status : "À Planifier",
+    details : [
+      { name : "Scénographies", progress : "0%"},
+      { name : "Storyboards", progress : "0%"},
+      { name : "Crayonnés", progress : "0%"},
+      { name : "Couleurs", progress : "0%"},
+      { name : "Finitions", progress : "0%"},
+    ]
+  },
+  {
+    title : "La Résistance [CJS-S1E5]",
+    cover : "assets/img/items/CJS-S1E5.webp",
+    status : "À Planifier",
+    details : [
+      { name : "Scénographies", progress : "0%"},
+      { name : "Storyboards", progress : "0%"},
+      { name : "Crayonnés", progress : "0%"},
+      { name : "Couleurs", progress : "0%"},
+      { name : "Finitions", progress : "0%"},
+    ]
+  },
+]
