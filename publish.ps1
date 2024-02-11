feb6394a7c1d81e5f8024645b539efb01f441877
marcgardent <marcgardent@yahoo.fr>
2024-02-11 17:52:59 +0100

    fix publish

--- publish.ps1
+++ publish.ps1
@@ -11,4 +11,4 @@ rm "$PsScriptRoot\docs" -Recurse
 mkdir "$PsScriptRoot\docs"
 Write-Output "ludopant.com" | Out-File "$PsScriptRoot\docs\CNAME" -Encoding utf8
 
-copy -Path  "$PsScriptRoot\sources\ludopant\dist\ludopant\browser\*.*" -Destination "$PsScriptRoot\docs" -Recurse
+copy -Path  "$PsScriptRoot\sources\ludopant\dist\ludopant\browser\*" -Destination "$PsScriptRoot\docs" -Recurse
